a3039a3469e9329529a4943b98313d92a6beed9c
kcmodev <shchristensen3@gmail.com>
2021-02-10 12:16:24 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cardiovascular Disease Detection App
+
+This app will use a random forest classifier machine learning algorithm to aid in detecting the likelihood a patient has cardiovascular disease.
+
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+user = {'name': 'Steve'}
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    index_title = 'Home'
+    return render_template('index.html', name=user['name'], title=index_title)
+
+
+# @app.route('/about')
+# def about():
+#     about_title = 'About Me'
+#     return render_template('about.html', name=user['name'], title=about_title)
+
+
+@app.route('/dashboard')
+def dashboard():
+    dashboard_title = 'Dashboard'
+    return render_template('dashboard.html', name=user['name'], title=dashboard_title)
+
+    # if request.method == 'POST':
+    #     # form_data = request.form
+    #     return render_template('dashboard.html', name=user['name'], title=dashboard_title)
+    # else:
+    #     return render_template('dashboard.html', name=user['name'], title=dashboard_title)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
