4342de428b459650519e77268f14fb896634a0ff
kcmodev <shchristensen3@gmail.com>
2021-03-09 13:25:02 -0600

    fixed config file

--- config.py
+++ config.py
@@ -6,9 +6,10 @@ class Config:
 
     FLASK_ENV = 'development'
     TESTING = True
-    SECRET_KEY = os.urandom(32)
+    # SECRET_KEY = os.urandom(32)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-    SQLALCHEMY_DATABASE_URI = postgres://foqldzvfnlnajd:332fa4754d8e2ea38de77b9e795bbd93ee5a641b245c0cbbacddf0aaac127562@ec2-52-44-31-100.compute-1.amazonaws.com:5432/d24h16792mcg0a
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = 'flask_app/static'
     TEMPLATES_FOLDER = 'flask_app/templates'
