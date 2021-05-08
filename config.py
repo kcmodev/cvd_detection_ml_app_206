5c9d927a1b50de8d03aa1a351d3d441215b89211
kcmodev <shchristensen3@gmail.com>
2021-05-08 14:17:44 -0500

    config variables fixed

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config:
     """Set Flask config variables."""
 
     # FLASK_ENV = 'development'
-    FLASK_DEBUG = 1
+    # FLASK_DEBUG = 1
     # TESTING = True
     SECRET_KEY = os.urandom(32)
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = 'flask_app/static'
     TEMPLATES_FOLDER = 'flask_app/templates'
